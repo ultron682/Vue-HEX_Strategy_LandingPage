2fb8c2254a68ef381e80b8a2f3bf3877ecbf82ed
ultron682 <heksandev@gmail.com>
2020-06-20 21:21:27 +0200

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
     <router-view/>
   </div>
 </template>
 
+<script>
+import Header from '@/components/Header/Header.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header
+  }
+}
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  /* Permalink - use to edit and share this gradient: https://colorzilla.com/gradient-editor/#3988d3+0,15d2d8+100 */
+  background: #3988d3; /* Old browsers */
+  background: -moz-linear-gradient(
+    left,
+    #3988d3 0%,
+    #15d2d8 100%
+  ); /* FF3.6-15 */
+  background: -webkit-linear-gradient(
+    left,
+    #3988d3 0%,
+    #15d2d8 100%
+  ); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(
+    to right,
+    #3988d3 0%,
+    #15d2d8 100%
+  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3988d3', endColorstr='#15d2d8',GradientType=1 ); /* IE6-9 */
+  color: white;
+  min-height: 100vh;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -11,12 +11,14 @@ const routes: Array<RouteConfig> = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/screens',
+    name: 'Screens',
+    component: () => import('../views/Screens-container.vue')
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact-container.vue')
   }
 ]
 
